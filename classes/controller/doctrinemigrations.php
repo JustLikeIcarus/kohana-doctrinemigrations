7b3550a3222a703d16c60f6dd4bad6cda5edddac
Kenny Kurtz <JustLikeIcarus@gmail.com>
2010-05-05 19:32:32 -0400

    Moving things around.

--- classes/controller/doctrinemigrations.php
+++ classes/controller/doctrinemigrations.php
@@ -19,7 +19,17 @@ class Controller_DoctrineMigrations extends Controller {
 	{
 		parent::before();
 		
-		
+		require Kohana::find_file('vendor', 'doctrine/lib/Doctrine');
+        spl_autoload_register(array('Doctrine', 'autoload'));
+        
+		$connection = Doctrine_Manager::connection
+    	(
+    		Kohana::config('database.default.type').'://'.
+    		Kohana::config('database.default.connection.username').':'.
+    		Kohana::config('database.default.connection.password').'@'.
+    		Kohana::config('database.default.connection.hostname').'/'.
+    		Kohana::config('database.default.connection.database')
+    	);
 	}
 
 	/**
--- init.php
+++ init.php
@@ -1,13 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-    require Kohana::find_file('vendor', 'doctrine/lib/Doctrine');
-    spl_autoload_register(array('Doctrine', 'autoload'));
     
-    $connection = Doctrine_Manager::connection
-	(
-		Kohana::config('database.default.type').'://'.
-		Kohana::config('database.default.connection.username').':'.
-		Kohana::config('database.default.connection.password').'@'.
-		Kohana::config('database.default.connection.hostname').'/'.
-		Kohana::config('database.default.connection.database')
-	);
+    
+    
