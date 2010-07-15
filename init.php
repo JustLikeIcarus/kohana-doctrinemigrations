cc71c563f8181bb63c6481b65fc7a53537fa5802
Mathew Davies <thepixeldeveloper@googlemail.com>
2010-07-15 22:00:06 +0100

    Remove doctrine from URL and renamed migrations to migrate.

--- init.php
+++ init.php
@@ -1,12 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-Route::set('doctrine', 'doctrine/migrations(/<version>)', array('version' => '\d+'))
+Route::set('doctrine', 'migrate(/<version>)', array('version' => '\d+'))
 	->defaults(array(
 		'controller' => 'migrations',
 		'directory'  => 'doctrine',
 	));
 
-Route::set('doctrine/current', 'doctrine/migrations/current')
+Route::set('doctrine/current', 'migrate/current')
 	->defaults(array(
 		'controller' => 'migrations',
 		'action'     => 'current',
